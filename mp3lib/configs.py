dd4eb550bda9d1bb28582b1418615cfb1d136c89
Mark Veltzer <mark.veltzer@gmail.com>
2019-02-01 14:50:15 +0200

    many improvements

--- mp3lib/configs.py
+++ mp3lib/configs.py
@@ -1,10 +1,25 @@
 from pytconf.config import ParamCreator, Config
 
 
-class ConfigFake(Config):
+class ConfigRepository(Config):
     """
-    Parameters to specify input files
+    Parameters to specify the repository
     """
-    input_files = ParamCreator.create_list_str(
-        help_string="input files",
+    folder = ParamCreator.create_existing_folder(
+        default="/mnt/seagate/mark/topics_archive/audio/abooks/by_name",
+        help_string="where is the repository",
+    )
+
+
+class ConfigNames(Config):
+    """
+    Parameters for names authorization repository
+    """
+    repo = ParamCreator.create_existing_file(
+        help_string="file where authorized strings are located",
+        default="/home/mark/.mp3lib_authorized_non_ascii.json",
+    )
+    all_over = ParamCreator.create_bool(
+        default=False,
+        help_string="start from scratch",
     )
--- mp3lib/endpoints/group_default.py
+++ mp3lib/endpoints/group_default.py
@@ -1,6 +1,8 @@
+import os
+
 from pytconf.config import register_endpoint, register_function_group
 
-from mp3lib.configs import ConfigFake
+from mp3lib.configs import ConfigRepository, ConfigNames
 
 GROUP_NAME_DEFAULT = "default"
 GROUP_DESCRIPTION_DEFAULT = "all pytsv commands"
@@ -13,9 +15,27 @@ def register_group_default():
     )
 
 
+def is_ascii(s, exceptions):
+    for c in s:
+        if ord(c) >= 128 and c not in exceptions:
+            return False
+    return True
+    # return all(ord(c) < 128 for c in s)
+
+
+def add_non_ascii(set_to_add_to, string_to_scan):
+    for c in string_to_scan:
+        if ord(c) >= 128:
+            set_to_add_to.add(c)
+
+
+def has_character(c, s):
+    return c in s
+
+
 @register_endpoint(
     configs=[
-        ConfigFake,
+        ConfigRepository,
     ],
     suggest_configs=[
     ],
@@ -26,4 +46,61 @@ def scan():
     """
     Scan the folder
     """
-    pass
+    bad_characters = set()
+    chars_ok_for_folders = ""
+    chars_ok_for_files = "“”ìú"
+    count = 0
+    for root, directories, files in os.walk(ConfigRepository.folder):
+        for file in files:
+            if 'á' in file:
+                full = os.path.join(root, file)
+                print(file, full)
+                count += 1
+            if not is_ascii(file, chars_ok_for_files):
+                # full = os.path.join(root, file)
+                # print(file, full)
+                add_non_ascii(bad_characters, file)
+        for directory in directories:
+            if not is_ascii(directory, chars_ok_for_folders):
+                full = os.path.join(root, directory)
+                print('folder [{}]'.format(full))
+    print(bad_characters)
+    print("found [{}] appearances".format(count))
+
+
+@register_endpoint(
+    configs=[
+        ConfigRepository,
+        ConfigNames,
+    ],
+    suggest_configs=[
+    ],
+    group=GROUP_NAME_DEFAULT,
+)
+def scan():
+    # type: () -> None
+    """
+    Authorize names with non ascii in them
+    Only checks for name not previously authorized
+    """
+    bad_characters = set()
+    chars_ok_for_folders = ""
+    chars_ok_for_files = "“”ìú"
+    count = 0
+    for root, directories, files in os.walk(ConfigRepository.folder):
+        for file in files:
+            if 'á' in file:
+                full = os.path.join(root, file)
+                print(file, full)
+                count += 1
+            if not is_ascii(file, chars_ok_for_files):
+                # full = os.path.join(root, file)
+                # print(file, full)
+                add_non_ascii(bad_characters, file)
+        for directory in directories:
+            if not is_ascii(directory, chars_ok_for_folders):
+                full = os.path.join(root, directory)
+                print('folder [{}]'.format(full))
+    print(bad_characters)
+    print("found [{}] appearances".format(count))
+
