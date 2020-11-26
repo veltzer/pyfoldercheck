0818fbcb95a77d5ccd1f7615673dacc060f222e5
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-26 13:53:43 +0200

    new version

--- pyfoldercheck/static.py
+++ pyfoldercheck/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.3"
+VERSION_STR = "0.0.4"
 DESCRIPTION = "apply a set of checks on files in a folder"
 APP_NAME = "pyfoldercheck"
 LOGGER_NAME = "pyfoldercheck"
