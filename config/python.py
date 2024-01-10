0993b62093874fa1bbcd5e1ae5e654551e26e836
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 14:36:30 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -4,16 +4,17 @@ from typing import List
 console_scripts: List[str] = [
     "pyfoldercheck=pyfoldercheck.main:main",
 ]
-config_requires: List[str] = []
 dev_requires: List[str] = [
     "pypitools",
+]
+config_requires: List[str] = [
+    "pyclassifiers",
 ]
 install_requires: List[str] = [
     "pytconf",
     "pylogconf",
 ]
 make_requires: List[str] = [
-    "pyclassifiers",
     "pymakehelper",
     "pydmt",
 ]
