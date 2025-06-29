e980c3f989e4fd1643dd5806223303a846618417
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:41 +0300

    fixed up deps

--- pyfoldercheck/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.6"
-DESCRIPTION = "Pyfoldercheck will apply a set of checks on files in a folder"
-APP_NAME = "pyfoldercheck"
-LOGGER_NAME = "pyfoldercheck"
